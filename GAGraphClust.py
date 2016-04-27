1d60c841fed18b985561af361928288113c81183
Cihan Kaya <cihank@Cihans-MacBook-Air.local>
2016-04-27 12:01:59 -0400

    changes in the README file

---
+++ GAGraphClust.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+
+''' Code for solving graph clustering with genetic algorithm
+	Author: Cihan Kaya
+'''
+
+import os
+import sys, getopt
+import numpy as np
+import argparse
+
+def calcAdj(graph, nodes):
+	adj = np.zeros((nodes, nodes))
+	for i in range(len(graph)):
+		adj[graph[i][0],graph[i][1]]=1.
+		adj[graph[i][1],graph[i][0]]=1.
+
+	return adj
+
+def calcDegrees(graph, nodes):
+	degrees=np.zeros(nodes,dtype=np.int)
+	for i in range(nodes):
+		degrees[i]=list(np.ravel(graph)).count(i)
+	return degrees
+	
+
+def objective(graph, adj, cluster, degrees, nodes, edges):
+	objective=0.
+	for i in range(nodes-1):
+		for j in range(i+1,nodes):
+			objective += ((adj[i,j]-degrees[i]*degrees[j]/(2.*edges))*(1.-(cluster[i]!=cluster[j])))
+	return objective
+
+def GAClust(graph,degrees,nodes,edges,adj,N,N_iter,alpha,beta,gamma):
+
+	N = int(N)
+	N_iter = int(N_iter)
+	alpha = int(alpha)
+	beta = float(beta)
+	gamma = float(gamma)
+	# Initialization
+	cluster = np.random.randint(N, size=(alpha,nodes))
+	objectives = np.zeros(alpha)
+	ne = int(alpha * beta)
+	nc = (alpha - ne)/2
+	pop1 = np.zeros((ne,nodes),dtype=np.int)
+	pop2 = np.zeros((alpha-ne,nodes),dtype=np.int)
+	best_obj = 0
+	best_sol = np.zeros(nodes, dtype=np.int)
+	m=0
+	sys.stdout.write("Running ")
+	sys.stdout.write("[%s]" % (" " * N_iter))
+	sys.stdout.flush()
+	sys.stdout.write("\b" * (N_iter+1))
+	
+	# fitness function evaluation for the first time
+	for j in range(cluster.shape[0]):
+		objectives[j] = objective(graph,adj,cluster[j],degrees,nodes,edges)
+
+	for i in range(N_iter):
+		# based on fitness function, selection of elite class
+		with np.errstate(invalid='ignore'):
+			rates = (objectives-min(objectives))/(np.sum(objectives-min(objectives)))
+			nums = np.random.choice(alpha,ne,p=rates)
+		pop1=cluster[nums]
+		# based on fitness function of elite class, cross over operations
+		rate_nums = rates[nums]/sum(rates[nums])
+		k=0
+		for j in range(int(nc)):
+			with np.errstate(invalid='ignore'):
+				Xa = cluster[np.random.choice(nums,p=rate_nums)]
+				Xb = cluster[np.random.choice(nums,p=rate_nums)]
+			changes = np.random.randint(nodes, size=np.random.randint(nodes))
+			Xc = Xa.copy()
+			Xd = Xb.copy()
+			for l in range(len(changes)):
+				Xc[changes[l]]=Xb[changes[l]]
+				Xd[changes[l]]=Xa[changes[l]]
+			pop2[k]=Xc
+			pop2[k+1]=Xd
+			k=k+2
+		# for new generated individuals mutation operations are performed
+		for j in range(int(nc)):
+			indice = np.random.randint(alpha-ne)
+			new = np.zeros(nodes, dtype=np.int)
+			for k in range(nodes):
+				new[k]=(pop2[indice,k]+np.random.randint(int(N))*(np.random.rand()<gamma))%N
+		cluster = np.concatenate((pop1,pop2),axis=0)
+		# new fitness function calculation
+		for j in range(cluster.shape[0]):
+			objectives[j] = objective(graph,adj,cluster[j],degrees,nodes,edges)
+		# if it is better than last seen best solution, update best solution
+		if max(objectives)>best_obj:
+			m=0
+			best_obj = max(objectives)
+			best_sol[:] = cluster[np.argmax(objectives==best_obj)]
+		# if the best solution stays for 20 iterations return best solution
+		if m==20:
+			return best_sol
+		sys.stdout.write("-")
+		sys.stdout.flush()
+	sys.stdout.write("\n")
+	return best_sol
+
+
+def main(argv):
+	N_iter = 100
+	N =5 
+	alpha = 1000
+	beta = 0.25
+	gamma = 0.1
+	filename = None
+	try:
+		opts, args = getopt.getopt(argv,"hi:N:n:a:b:g:",["filename=","NumClass=","N_iter=","alpha=","beta=","gamma="])
+	except getopt.GetoptError:
+		print 'Usage: python GAGraphClust.py -i <filename> [-N <MaxNumClass>] [-n <MaxNumIteration>] [-a <PopulationSize>] [-b <ElitismParameter>] [-g <MutationRate>]'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'Usage: python GAGraphClust.py -i <filename> [-N <MaxNumClass>] [-n <MaxNumIteration>] [-a <PopulationSize>] [-b <ElitismParameter>] [-g <MutationRate>]'
+			sys.exit()
+		elif opt in ("-i", "--filename"):
+		 	filename = arg
+		elif opt in ("-N", "--NumClass"):
+			N = arg
+		elif opt in ("-n", "--N_iter"):
+			N_iter = arg
+		elif opt in ("-a", "--alpha"):
+			alpha = arg
+		elif opt in ("-b", "--beta"):
+			beta = arg
+		elif opt in ("-g", "--gamma"):
+			gamma = arg
+	if filename == None:
+		print 'Usage: python GAGraphClust.py -i <filename> [-N <MaxNumClass>] [-n <MaxNumIteration>] [-a <PopulationSize>] [-b <ElitismParameter>] [-g <MutationRate>]'
+                sys.exit(2)
+
+	with open(filename, 'r') as f:
+		nodes, edges = f.readline().split(',')
+	# parse input from graph and determine number of nodes and edges 
+	nodes=int(filter(type(nodes).isdigit,nodes))
+	edges=int(filter(type(edges).isdigit,edges))
+	data=np.loadtxt(filename, dtype=np.int)
+	# calculate adjacency matrix
+	adj=calcAdj(data,nodes)
+	# precalculate degrees of each nodes
+	degrees = calcDegrees(data, nodes)
+	# call genetic algorithm to get the best cluster
+	best_clust = GAClust(data,degrees,nodes,edges,adj,N,N_iter,alpha,beta,gamma)
+	# get the fitness function value of best cluster 
+	obj = objective(data, adj, best_clust, degrees, nodes, edges)
+
+	# print output to the standart out
+	clusters = np.unique(best_clust)
+	members = []
+	print "#Modularity " + str(obj)
+	for i in range(len(clusters)):
+		members.append([])
+	for i in range(nodes):
+		members[np.where(best_clust[i]==clusters)[0][0]].append(i)
+
+	for i in range(len(members)):
+		print "Module " + str(i+1) + ":\t",
+		for j in range(len(members[i])):
+			print str(members[i][j]) + "\t",
+		print ""
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # GAClust
+
+SYNOPSIS
+
+GAGraphClust is a method to cluster graphs to optimize modularity of the graph. It uses a genetic algorithm
+with various operations such as cross-over, mutation and elitist selection. Individual parameters for each
+operation are defined in the following sections.
+
+PARAMETERS
+
+filename: The filename of the graph input file.
+
+The first line of this file begins with a hash ‘#’ followed by the
+number of nodes and edges in the following format:
+#n=34,m=78
+Each following line is tab-delimited and contains the two end-points of each edge:
+1 0
+2 0
+...
+33 32
+In general, if there are m edges in the graph, there should be exactly m + 1 lines in the file. All
+graphs are undirected and edges are repeated only once (e.g. in the above example, there will not
+be a second line with ‘0 1’ since this is the same as the first line for undirected graphs).
+
+maximum number of clusters: For initial generation of clusters, a maximum number of clusters needs to be suggested.
+This parameter is used to limit computational requirement of the algorithm. This parameter is optional. Default value is
+5.
+
+maximum number of iterations: iterations are converged if the best individual will not change more than 20 iterations.
+To guarantee exit of program, a maximum number of iterations is defined. This parameter is optional. Default value is
+100.
+
+population size: the number of individuals in each population. This parameter is optional. Default value is 1000.
+
+elitist selection parameter: the ratio of the individuals with high fitness carried over to next generations.
+This parameter is optional. Default value is 0.25.
+
+mutation rate: the rate of mutation for each element in the individual vectors. This parameter is optional. Default value is 0.1
+
+USAGE
+
+Usage: python setup.py -i <filename> [-N <MaxNumClass>] [-n <MaxNumIteration>] [-a <PopulationSize>] [-b <ElitismParameter>] [-g <MutationRate>]
